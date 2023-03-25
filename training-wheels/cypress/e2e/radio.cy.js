0999770d3a69fc6068fcde65861132d88947b204
Tiago Dias <tiagooliveira@bemol.com.br>
2023-03-25 13:57:39 -0400

    Update project

--- training-wheels/cypress/e2e/radio.cy.js
+++
@@ -1,8 +0,0 @@
-it('Deve marcar o radio button', function(){
-    cy.visit('/radios')
-
-    
-    cy.get('input[value=thor3]')
-    .click()
-    .should('be.checked')
-})
--- training-wheels/cypress/e2e/timeout.cy.js
+++
@@ -1,9 +0,0 @@
-it('Deve verificar o time', function () {
-    cy.visit('/timeout')
-
-    cy.contains('button', 'Habilita').click()
-
-    cy.get('#firstname')
-        .should('be.visible')
-        .type('Tiago Dias', {timeout: 8000})
-})
